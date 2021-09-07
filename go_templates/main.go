d5e63f5475bf33647035191cbe58d5aae7dedf00
Juan Ramos <juan-rb01@hotmail.com>
2021-09-07 08:40:40 -0600

    go templates

--- go_templates/main.go
+++ go_templates/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"html/template"
 	"os"
-	"text/template"
 )
 
+type Person struct {
+	Name    string
+	Age     int
+	Hobbies []string
+}
+
+var funcs = template.FuncMap{
+	"increment": func(num int) int {
+		return num + 1
+	},
+}
+
 func main() {
-	name := "John"
-	t, err := template.New("greeting").Parse("Hello {{.}}, nice to see you")
+
+	john := &Person{"John", 25, []string{"<p>asdsad</p>", "ver peliculas", "programar"}}
+	loadTemplate("index.html", john)
+}
+
+func loadTemplate(fileName string, data interface{}) {
+	t, err := template.New(fileName).Funcs(funcs).ParseFiles("templates/" + fileName)
+
 	if err != nil {
 		panic(err)
 	}
 
-	err = t.Execute(os.Stdout, name)
+	err = t.Execute(os.Stdout, data)
 	if err != nil {
 		panic(err)
 	}
